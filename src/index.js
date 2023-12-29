1706e3b588cb3d6c32813b9284ba5616c70b9565
Hugo Gallardo <hgallardo@checkpt.com>
2023-12-29 11:55:29 -0600

    feat: adding load login

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const nameVariable = document.getElementById("name-variable");
 const valueVariable = document.getElementById("value-variable");
-const submitButton = document.getElementById( "submit-button" );
+const modifyButton = document.getElementById( "submit-button" );
 const checkButton = document.getElementById( "check-button");
+const addButton = document.getElementById( "add-button");
+const container = document.getElementById("container");
 
 function loadVariablesFile(){
     cockpit.file("/tmp/environment/file-env-vars.txt").read()
@@ -10,12 +12,20 @@ function loadVariablesFile(){
         var data = content.split("\n");
         data.forEach(element => {
            if(element){
-            console.log(element)
-            console.log("**************")
             values =  element.split("=")
             nameVariable.value = values[0]
-            valueVariable.value = values[1]
+            valueVariable.value = values[1].replace("'","")
+            var inputName = document.createElement("input");
+            var inputValue = document.createElement("input");
+            inputName.type = "text";
+            inputName.name = nameVariable;
+            inputName.size = 55
+            inputValue.type = "text";
+            inputValue.name = nameVariable;
+            inputValue.size = 55
 
+            container.appendChild(inputName);
+            container.appendChild(inputValue);
            }
             
         });
@@ -25,15 +35,16 @@ function loadVariablesFile(){
        
     })
     .catch(error => {
-        console.log("xxxxxxxxxxxxxxxxx")
         console.log(error)
     });
 }
 
 function submit(){
    
-    cockpit.script("echo " + nameVariable.value.toUpperCase() + "=" + valueVariable.value + " >> /tmp/environment/file-env-vars.txt");
-
+    cockpit.script("echo " + nameVariable.value.toUpperCase() + "=" + valueVariable.value.toUpperCase() + " >> /tmp/environment/file-env-vars.txt");
+    loadVariablesFile()
 }
+
+ 
 checkButton.addEventListener("click", loadVariablesFile);
 submitButton.addEventListener("click", submit);
